995c76ecb32d1f36cb45744264f86dfc3baa384a
Rogliano Antoine <inateno@gmail.com>
2018-03-14 12:11:39 +0100

    fix etape 2-3

--- public/index.js
+++ public/index.js
@@ -17,7 +17,7 @@ function generateGrid( maxX, maxY )
     var line = [];
     for ( var x = 0; x < maxX; ++x )
     {
-      line.push( Math.random() * 10 > 7 ? 1 : 0 );
+      line.push( Math.random() * 10 > 8 ? 1 : 0 );
     }
     grid.push( line );
   }
@@ -50,33 +50,76 @@ function render( ctx, grid )
   }
 }
 
+function aliveNb( grid, cellX, cellY ) {
+  let nb = 0;
+
+  for ( var y = cellY - 1; y <= cellY + 1; ++y )
+  {
+    if ( y < 0 || y >= grid.length ) {
+      continue;
+    }
+
+    for ( var x = cellX - 1; x <= cellX + 1; ++x )
+    {
+      if ( ( x == cellX && y == cellY )
+        || x < 0 || x >= grid[ 0 ].length ) {
+        continue;
+      }
+
+      if ( grid[ y ][ x ] ) {
+        ++nb;
+      }
+    }
+  }
+
+  return nb;
+}
+
+function updateCellState( grid, y, x )
+{
+  var nb = aliveNb( grid, y, x );
+  var currentState = grid[ y ][ x ];
+  if ( currentState == 1 ) {
+    if ( nb >= 2 && nb <= 3 ) {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+  else {
+    return nb == 3 ? 1 : 0;
+  }
+}
+
+function updateGridState( grid )
+{
+  var newGrid = [];
+  for ( var y = 0; y < grid.length; ++y )
+  {
+    newGrid[ y ] = [];
+    for ( var x = 0; x < grid[ y ].length; ++x )
+    {
+      newGrid[ y ][ x ] = updateCellState( grid, y, x );
+    }
+  }
+
+  return newGrid;
+}
+
+var gridhistory = [];
 // should update the grid
 function update( ctx, grid )
 {
-  // do update
-
-    // on verifie à l'arrache que ça calcule pas trop mal...
-    console.log(getVoisinsVivants(maxX -1,maxY-1,grid));
-  
+  gridhistory.push( grid );
   render( ctx, grid );
-}
-
-function getVoisinsVivants(x, y, grid) {
-    let nbVoisinsVivants = 0;
-
-    for (let i = Math.max(x - 1, 0); i <= Math.min(x + 1, maxX - 1); i++) {
-        for (let j = Math.max(y - 1, 0); j <= Math.min(y + 1, maxY - 1); j++) {
-            if (i !== x || j !== y) {
-                nbVoisinsVivants += grid[i][j];
-            }
-        }
-    }
-
-    return nbVoisinsVivants;
+  _grid = updateGridState( grid );
+  
+  console.log( "update" )
 }
 
 var _grid = generateGrid( maxX, maxY );
 setInterval( function()
 {
   update( CTX, _grid );
-}, 500 /* 500 ms */ );
+}, 1000 /* 500 ms */ );
