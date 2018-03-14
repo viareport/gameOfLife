5b1516b3cc44df1ce419ab9d54bd1187f5ca64ac
lgra <grasset@free.fr>
2018-03-14 10:54:39 +0100

    rien

--- src/Add.js
+++ src/Add.js
@@ -3,3 +3,16 @@
 const add = (n1, n2) => n1 + n2;
 
 module.exports = {add};
+
+const biotop = {
+  rows: 10,
+  cols: 10,
+
+  matrice: [],
+
+  init: (r,c) => {
+    rows = r
+    cols = c
+  }
+
+}
--- src/Application.js
+++ src/Application.js
@@ -1,13 +1,14 @@
 "use strict";
 
-let {add} =  require("./Add");
+let {add} =  require("./Add")
 
-let args = process.argv.slice(2);
+let args = process.argv.slice(2)
 
 function main(args) {
-    let n1 = Number(args[0]);
-    let n2 = Number(args[1]);
+    let n1 = Number(args[0]) || 10
+    let n2 = Number(args[1]) || 10
     console.log(`Resultat ${add(n1, n2)}`)
 }
 
-main(args);
+main(args)
+
