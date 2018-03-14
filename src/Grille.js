96e954338a4edd5bbe6de5e507f15ba28b55a5ae
Unknown <guillaume.sayegh@gmail.com>
2018-03-14 12:53:02 +0100

    etape 4

--- src/Grille.js
+++ src/Grille.js
@@ -1,65 +1,89 @@
 "use strict";
 
-function initCells(dimX, dimY) {
-    const grille = Array(dimX);
-    for (let x = 0 ; x < grille.length ; x++) {
-        grille[x] = Array(dimY);
+function initCells(dimX, dimY, alive) {
+    const grille = new Map();
+    for (let x = 0 ; x < dimX ; x++) {
 
-        for (let y = 0 ; y < grille[x].length ; y++) {
-            grille[x][y] = CELL.EMPTY;
+        for (let y = 0 ; y < dimY ; y++) {
+          var coordonnees = {x,y};
+          var cellValue = alive.some(coor => coor.x === x && coor.y === y) ? CELL.ALIVE : CELL.DEAD;
+            grille.set(coordonnees, cellValue);
         }
     }
     return grille;
 }
 
 const CELL = {
-    EMPTY: " ",
-    DEAD: "x",
-    ALIVE: "o"
+    DEAD: false,
+    ALIVE: true
 };
 
 class Grille {
-    constructor(dimX, dimY) {
+    constructor(dimX, dimY, alive = []) {
         this.dimX = dimX;
         this.dimY = dimY;
-        this.cells = initCells(dimX, dimY);
+        this.cells = initCells(dimX, dimY, alive);
     }
 
-    getCell(x, y) {
-       // if (x < 0 || x > this.cells.length || (this.cells[x]))
-        return this.cells[x][y];
-    }
+    // getCell(x, y) {
+    //    // if (x < 0 || x > this.cells.length || (this.cells[x]))
+    //     return this.cells[x][y];
+    // }
 
-    setCell(x, y, content) {
-        this.cells[x][y] = content;
-    }
-
-    toString() {
-        const grille = this.cells;
-        let result = "";
-        for (let x = 0 ; x < grille.length ; x++) {
-
-            for (let y = 0 ; y < grille[x].length ; y++) {
-                result += grille[x][y];
-            }
-            result += "\n";
-        }
-        return result;
-    }
-  
-  nbrAliveVoisin(x, y) {
-    var nbr = 0
-    for (var i = (cell.x - 1); i <= (cell.x + 1); i++) {
-      for (var j = (cell.y - 1); j <= (cell.y + 1); j++) {
-        if ((i >= 0) && (j >= 0) && (i < dimX) && (j < dimY)) {
-          if (this.cells[x] && this.cells[x][y] && (this.cells[x][y].state === 1)) {
-            nbr++
-          }
-        }
+    alive() {
+      var nb = 0;
+      for (var cell of this.cells.values()) {
+        nb += cell ? 1 : 0;
       }
+      return nb;
     }
-    return nbr
-  }
+
+    dead() {
+      var nb = 0;
+      for (var cell of this.cells.values()) {
+        nb += cell ? 0 : 1;
+      }
+      return nb;
+    }
+
+    celluleVivantesAutour({x,y}) {
+      return Array.from(this.cells.keys())
+      .filter(({dx, dy}) => (dx === x-1 || dx === x+1) && (dy === y-1 || dy === y+1) )
+      .filter(({dx, dy}) => this.cells.get({x: dx, y: dy}) === true).length;
+    }
+
+    // setCell(x, y, content) {
+    //     this.cells[x][y] = content;
+    // }
+    //
+    // toString() {
+    //     const grille = this.cells;
+    //     let result = "";
+    //     for (let x = 0 ; x < grille.length ; x++) {
+    //
+    //         for (let y = 0 ; y < grille[x].length ; y++) {
+    //             result += grille[x][y];
+    //         }
+    //         result += "\n";
+    //     }
+    //     return result;
+    // }
+
+
+    // nbrAliveVoisin(x, y) {
+    //   var nbr = 0
+    //   for (var i = (cell.x - 1); i <= (cell.x + 1); i++) {
+    //     for (var j = (cell.y - 1); j <= (cell.y + 1); j++) {
+    //       if ((i >= 0) && (j >= 0) && (i < dimX) && (j < dimY)) {
+    //         if (this.cells[x] && this.cells[x][y] && (this.cells[x][y].state === 1)) {
+    //           nbr++
+    //         }
+    //       }
+    //     }
+    //   }
+    //   return nbr
+    // }
+
 }
 
 module.exports = {Grille, CELL};
--- test/Application.test.js
+++ test/Application.test.js
@@ -9,28 +9,31 @@ describe("Grille", () => {
    it("Par défaut, une grille n'a aucune cellule vivante", () => {
      const dimX = 3, dimY = 4;
      var grille = new Grille(dimX, dimY);
-     expect(grille.cells.alive.length).toBe(0);
+     expect(grille.alive()).toBe(0);
    })
    it("Par défaut une grille n'a aucune cellule morte", () => {
      const dimX = 3, dimY = 4;
      var grille = new Grille(dimX, dimY);
-     expect(grille.cells.dead.length).toBe(0);
+     expect(grille.dead()).toBe(dimX * dimY);
    })
-    it("Par défaut une grille a toutes ces cellules vides", () => {
-        const dimX = 3, dimY = 4;
-        var grille = new Grille(dimX, dimY);
-        expect(grille.cells.empty.length).toBe(dimX * dimY);
-    })
-   // it("On peut disposer des cellules vivantes dans la grille", () => {
-   //   const dimX = 3, dimY = 4;
-   //   const alive = new Map();
-   //   alive.set()
-   //   var grille = new Grille(dimX, dimY, alive);
-   //   expect(grille.cells.length.toBe(dimX * dimY);
-   // })
-   it("on ne sait pas tester puisque l'on a créer la grille en aléatoire", () => {
-    const dimX = 3, dimY = 4;
-    var grille = Grille(dimX, dimY);
-    expect(grille.cells.size).toBe(dimX * dimY);
+   it("On peut disposer des cellules vivantes dans la grille", () => {
+     const dimX = 3, dimY = 4;
+     const alive = [
+       {x: 1, y: 1},
+       {x: 1, y: 2},
+       {x: 2, y: 1}
+     ];
+     var grille = new Grille(dimX, dimY, alive);
+     expect(grille.alive()).toBe(3);
+   })
+   it("déterminer pour n’importe quelle cellule de la grille son nombre de voisins vivants", () => {
+     const dimX = 3, dimY = 4;
+     const alive = [
+       {x: 1, y: 1},
+       {x: 1, y: 2},
+       {x: 2, y: 1}
+     ];
+     var grille = new Grille(dimX, dimY, alive);
+     expect(grille.celluleVivantesAutour({ x: 1, y: 1 })).toBe(2);
   })
 });
