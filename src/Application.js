e4e9008a288dcaff3a5cdf78266f4d9b5f152d07
lgra <grasset@free.fr>
2018-03-14 11:45:43 +0100

    Etape 3

--- src/Application.js
+++ src/Application.js
@@ -1,6 +1,7 @@
 "use strict";
 
-let {add} =  require("./Add");
+let { add } = require("./Add");
+let { Grille } = require("./Grille");
 
 let args = process.argv.slice(2);
 
@@ -8,6 +9,9 @@ function main(args) {
     let n1 = Number(args[0]);
     let n2 = Number(args[1]);
     console.log(`Resultat ${add(n1, n2)}`)
+
+    let grille = new Grille(n1, n2)
+    grille.afficher()
 }
 
 main(args);
--- src/Grille.js
+++ src/Grille.js
@@ -1,61 +1,72 @@
+const cellStatus = {
+  empty: 0,
+  alive: 1,
+  dead: 2
+}
+
 
 class Grille {
 
-    constructor(nbLignes, nbColonnes) {
-        this.nbLignes = nbLignes;
-        this.nbColonnes = nbColonnes;
-        this.grille = [];
-        for(let i = 0; i < nbLignes; i++) {
-            for(let j = 0; j < nbColonnes; j++) {
-                if(!this.grille[i]) {
-                    this.grille[i] = [];
-                }
-                this.grille[i][j] = this.getRandomInt(2);
-            }
+  constructor(nbLignes, nbColonnes) {
+    this.nbLignes = nbLignes;
+    this.nbColonnes = nbColonnes;
+    this.grille = [];
+    for (let i = 0; i < nbLignes; i++) {
+      for (let j = 0; j < nbColonnes; j++) {
+        if (!this.grille[i]) {
+          this.grille[i] = [];
         }
-
+        this.grille[i][j] = this.getRandomInt(3); // 0 - vide, 1 - alive, 2 - morte
+      }
     }
 
-    getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  afficher() {
+    let affichage = this.grille.map((row) => row.map((cell) => cell === cellStatus.alive ? 'A' : cell === cellStatus.dead ? 'd' : '-').join("\t")).join("\n")
+    console.log(affichage);
+  }
+
+  getCellule(ligne, colonne) {
+    if (!this.grille[ligne] || !this.grille[ligne][colonne]) {
+      return null;
     }
+    return this.grille[ligne][colonne];
+  }
 
-    afficher() {
-        let affichage = "";
-        for(let i = 0; i < this.nbLignes; i++) {
-            for(let j = 0; j < this.nbColonnes; j++) {
-                affichage = affichage +this.grille[i][j];
+  cellulesVoisinesVivantes(ligne, colonne) {
 
-            }
-            affichage += "\n";
-        }
-        console.log(affichage);
-    }
+    var nb = 0;
 
-    getCellule(ligne, colonne) {
-        if(!this.grille[ligne] || !this.grille[ligne][colonne]) {
-            return null;
-        }
-        return this.grille[ligne][colonne];
-    }
+    nb += this.getCellule(ligne - 1, colonne - 1) === cellStatus.alive ? 1 : 0;
+    nb += this.getCellule(ligne - 1, colonne) === cellStatus.alive ? 1 : 0;
+    nb += this.getCellule(ligne - 1, colonne + 1) === cellStatus.alive ? 1 : 0;
+    nb += this.getCellule(ligne, colonne - 1) === cellStatus.alive ? 1 : 0;
+    nb += this.getCellule(ligne, colonne + 1) === cellStatus.alive ? 1 : 0;
+    nb += this.getCellule(ligne + 1, colonne - 1) === cellStatus.alive ? 1 : 0;
+    nb += this.getCellule(ligne + 1, colonne) === cellStatus.alive ? 1 : 0;
+    nb += this.getCellule(ligne + 1, colonne + 1) === cellStatus.alive ? 1 : 0;
 
-    cellulesVoisinesVivantes(ligne, colonne) {
+    return nb;
+  }
 
-      var nb = 0;
-
-      nb += this.getCellule(ligne - 1, colonne - 1) ? 1 : 0;
-      nb += this.getCellule(ligne - 1, colonne) ? 1 : 0;
-      nb += this.getCellule(ligne - 1, colonne + 1) ? 1 : 0;
-      nb += this.getCellule(ligne, colonne -1) ? 1 : 0;
-      nb += this.getCellule(ligne, colonne + 1) ? 1 : 0;
-      nb += this.getCellule(ligne + 1, colonne - 1) ? 1 : 0;
-      nb += this.getCellule(ligne + 1, colonne) ? 1 : 0;
-      nb += this.getCellule(ligne + 1, colonne + 1) ? 1 : 0;
-
-      return nb;
-    }
+  generateNextState() {
+    this.grille = this.grille.map((row, r) => row.map((cell, c) => {
+      var nbvoisin = cellulesVoisinesVivantes(r, c)
+      if (nbvoisin < 2 || nbvoisin > 3) {
+        return cellStatus.dead
+      }
+      else {
+        return cell
+      }
+    }))
+  }
 
 
 }
 
-module.exports = {Grille};
+module.exports = { Grille };
